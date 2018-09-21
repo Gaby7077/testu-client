ccbf613d3f521324f4c842ad3d8f72ee8088b93a
abelardocbc <abelardocbc@gmail.com>
2018-09-20 19:38:10 -0500

    Final

--- testu/testu/src/views/Signup/Signup.js
+++ testu/testu/src/views/Signup/Signup.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import "./Signup.css";
 import UserAPI from "../../api/user.api";
+import Button from "components/MaterialButton/MaterialButton.jsx";
+import { Grid, Row, Col, FormGroup, FormControl, ControlLabel, Modal } from "react-bootstrap";
 
 
 class Signup extends Component {
@@ -28,9 +30,23 @@ class Signup extends Component {
                 empresa: this.state.empresa
             })
             .then(response => {
+                this.setState({
+                    showPregunta: true,
+                })
                 console.log(response)
                 //window.location.replace("/login")
             })
+    }
+
+    handleClosePregunta = event => {
+        event.preventDefault();
+                this.setState({
+                    username: "",
+                    password: "",
+                    empresa: "",
+                    showPregunta: false,
+                })
+                
     }
 
     logout = event => {
@@ -57,15 +73,15 @@ class Signup extends Component {
                             <form className="login">
                                 <div className="form-group">
                                     <label htmlFor="exampleInputEmail1">Email address</label>
-                                    <input type="email" onChange={this.handleInputChange} name="username" className="form-control" id="email-input" placeholder="Email" />
+                                    <input type="email" onChange={this.handleInputChange} name="username" className="form-control" id="email-input"  value={this.state.username} placeholder="Email" />
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="Empresa">Empresa</label>
-                                    <input type="text" onChange={this.handleInputChange} name="empresa" className="form-control" id="empresa-input" placeholder="Empresa" />
+                                    <input type="text" onChange={this.handleInputChange} name="empresa" className="form-control" id="empresa-input" value={this.state.empresa}  placeholder="Empresa" />
                                 </div>
                                 <div className="form-group">
                                     <label htmlFor="exampleInputPassword1">Password</label>
-                                    <input type="password" className="form-control" name="password" onChange={this.handleInputChange} id="password-input" placeholder="Password" />
+                                    <input type="password" className="form-control" name="password" onChange={this.handleInputChange} id="password-input" value={this.state.password} placeholder="Password" />
                                 </div>
                                 <button type="submit" onClick={this.handleFormSubmit} className="btn btn-default">Dar de Alta</button>
                                 
@@ -77,6 +93,15 @@ class Signup extends Component {
 
                     </div>
                 </div>
+                <Modal show={this.state.showPregunta} onHide={this.handleClosePregunta}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Usuario Agregado</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Footer>
+                    <Button onClick={this.handleClosePregunta}>Close</Button>
+                </Modal.Footer>
+            </Modal>
             </div>
 
 
